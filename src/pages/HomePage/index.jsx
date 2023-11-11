efcc64482d8d4969d77df948734d778bfcee1101
Týna Wolo <tyna@Tynas-MacBook-Air.local>
2023-11-11 21:41:35 +0100

    Ticket reservation functionality added

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,68 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
+import { SelectedSeat } from '../../components/SelectedSeat';
+import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
   const [journeyPlan, setJourneyPlan] = useState(null);
+  const [reservationId, setReservationId] = useState(null);
+
   const handleJourneyChange = (journeyEntry) => {
     console.log(journeyPlan);
     setJourneyPlan(journeyEntry);
   };
   console.log(journeyPlan);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const onTicketBuy = async () => {
+      if (journeyPlan && journeyPlan.autoSeat && journeyPlan.journeyId) {
+        const response = await fetch(
+          'https://apps.kodim.cz/daweb/leviexpress/api/reservation',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              action: 'create',
+              seat: journeyPlan.autoSeat,
+              journeyId: journeyPlan.journeyId,
+            }),
+          },
+        );
+
+        const data = await response.json();
+
+        setReservationId(data.results.reservationId);
+      }
+    };
+    onTicketBuy();
+  }, [journeyPlan]);
+
+  const handleBuy = () => {
+    console.log('reservation', reservationId);
+  };
+  useEffect(() => {
+    if (reservationId) {
+      navigate(`/reservation/${reservationId}`);
+    }
+  }, [reservationId]);
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
-      <h2>
-        {journeyPlan === null
-          ? 'Zadejte spoj k vyhledání'
-          : `Naleznen spoj ID: ${journeyPlan.journeyId}`}
-      </h2>
-      {journeyPlan === null ? (
-        'Zadejte spoj k vyhledání'
-      ) : (
-        <JourneyDetail journey={journeyPlan} />
+      {journeyPlan === null ? null : (
+        <>
+          <JourneyDetail journey={journeyPlan} />
+          <SelectedSeat number={journeyPlan.autoSeat} />
+
+          <div className="controls container">
+            <button onClick={handleBuy} className="btn btn--big" type="button">
+              Rezervovat
+            </button>
+          </div>
+        </>
       )}
     </main>
   );
--- src/pages/Reservation/index.jsx
+++ src/pages/Reservation/index.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 export const Reservation = () => {
+  const [reservation, setReservation] = useState(null);
+  let { id } = useParams();
+  console.log('id:', id);
+
+  useEffect(() => {
+    const fetchReservationData = async () => {
+      const response = await fetch(
+        `https://apps.kodim.cz/daweb/leviexpress/api/reservation?id=${id}`,
+      );
+      const data = await response.json();
+      setReservation(data.results);
+    };
+    fetchReservationData();
+    console.log('reservation data', reservation);
+  }, []);
+  console.log('rezervace', reservation);
+
   return (
     <main>
-      <h1>Tady bude rezervace</h1>
+      <div className="reservation container">
+        <h2>Vaše e-jízdenka č. {id}</h2>
+        <div className="reservation__body">
+          <div className="reservation__headings">
+            <p>Datum cesty:</p>
+            <p>Odjezd:</p>
+            <p>Příjezd:</p>
+            <p>Sedadlo:</p>
+          </div>
+          <div className="reservation__info">
+            <p>{reservation !== null ? reservation.date : null}</p>
+            <p>
+              {reservation !== null ? reservation.fromCity.name : null}{' '}
+              {reservation !== null ? reservation.fromCity.time : null}
+            </p>
+            <p>
+              {reservation !== null ? reservation.toCity.name : null}{' '}
+              {reservation !== null ? reservation.toCity.time : null}
+            </p>
+            <p>{reservation !== null ? reservation.seatNumber : null}</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
